b5d02a834784d86637629d68c08c720e73a59d12
vishwakarmashyamchand1-sudo <vishwakarmashyamchand1@gmail.com>
2026-06-10 15:12:31 +0530

    Connect frontend with render backend

--- Backend/server.js
+++ Backend/server.js
@@ -13,10 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-// Allow all local ports in development (so Vite on 5173, 5174 etc won't fail)
-if (process.env.NODE_ENV !== "production") {
-  app.use(cors());
-}
+// Allow all origins so Vercel frontend can communicate with Render backend
+app.use(cors());
 
 app.use(express.json()); // parse JSON bodies
 app.use(rateLimiter);
