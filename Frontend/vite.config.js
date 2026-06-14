d500092e6865dc68a3a8775304632d605588d970
vishwakarmashyamchand1-sudo <vishwakarmashyamchand1@gmail.com>
2026-06-14 11:06:15 +0530

    Added PWA app shortcuts

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,28 +5,49 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-
     VitePWA({
       registerType: "autoUpdate",
-
       manifest: {
         name: "Todo Notes App",
         short_name: "TodoApp",
-
         description: "My Todo Notes PWA",
-
         theme_color: "#0f172a",
         background_color: "#0f172a",
-
         display: "standalone",
-
+        shortcuts: [
+          {
+            name: "Add Note",
+            short_name: "Add",
+            description: "Create a new note",
+            url: "/create",
+            icons: [
+              {
+                src: "/pwa-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+              },
+            ],
+          },
+          {
+            name: "Search Notes",
+            short_name: "Search",
+            description: "Search notes",
+            url: "/",
+            icons: [
+              {
+                src: "/pwa-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+              },
+            ],
+          },
+        ],
         icons: [
           {
             src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
-
           {
             src: "/pwa-512x512.png",
             sizes: "512x512",
